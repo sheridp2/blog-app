005719037b426ccdf8269738b55773ab99ce6cd8
Patrick Sheriddan <sheridp2@gmail.com>
2018-12-04 13:30:35 -0800

    form reducer added, redux-from added, PostNew component added, title, categories, content fields added for form and rendering via renderfield comonent, error messages added if field is empty

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
 
 class PostNew extends Component {
-  render(){
-    return(
-      <div>
-        PostsNew!
+
+  renderField(field){
+    return (
+      <div className="form-group">
+      <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        {field.meta.error}
       </div>
     )
   }
+
+  render(){
+    return(
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    )
+  }
 }
- export default PostNew;
+
+function validate(values){
+  const errors = {};
+  if(!values.title || values.title.length < 3){
+    errors.title="Enter a title this is at least 3 charaters long!";
+  }
+
+  if(!values.categories ){
+    errors.categories="Enter some categories";
+  }
+  if(!values.content){
+    errors.content="Enter some content";
+  }
+  return errors;
+}
+
+ export default reduxForm({
+   validate,
+   form: 'PostsNewForm'
+ })(PostNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
-import { reducer as fromReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form'
 import PostReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostReducer
+  posts: PostReducer,
+  form: formReducer
 
 });
 
