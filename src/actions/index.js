f5bb2e41ddc7c6fff5a7c3d3351d984a48bc08e1
Patrick Sheriddan <sheridp2@gmail.com>
2018-12-04 12:07:53 -0800

    .

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts'
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
-const RANDOM_STRING = '?key=PATSKEY123'
+// const RANDOM_STRING = '?key=PATSKEY123'
 
 export function fetchPosts(){
   const request = axios.get(`${ROOT_URL}/posts${RANDOM_STRING}`);
