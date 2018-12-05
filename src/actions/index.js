d347aa25f41d514bb2ed5a08abe73ab8cbb0a4bf
Patrick Sheriddan <sheridp2@gmail.com>
2018-12-04 18:35:11 -0800

    delete post added

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts'
 export const FETCH_POST = 'fetch_post'
 export const CREATE_POST = 'create_post'
+export const DELETE_POST = 'delete_post'
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const RANDOM_STRING = '?key=PATSKEY123'
@@ -33,5 +34,14 @@ export function fetchPost(id){
   return{
     type: FETCH_POST,
     payload: request
+  }
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${RANDOM_STRING}`)
+    .then(() => callback())
+  return{
+    type: DELETE_POST,
+    payload: id
   }
 }
