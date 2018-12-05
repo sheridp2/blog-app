6a3d96783e21f3c36f2a212781ffc0243ff9ec3f
Patrick Sheriddan <sheridp2@gmail.com>
2018-12-04 18:11:36 -0800

    create post, get post actions created, list of post now updates after successful submission of new post, individual post can now be clicked on to view one post

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 
 export const FETCH_POSTS = 'fetch_posts'
+export const FETCH_POST = 'fetch_post'
+export const CREATE_POST = 'create_post'
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const RANDOM_STRING = '?key=PATSKEY123'
 
@@ -10,6 +13,25 @@ export function fetchPosts(){
 
   return{
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${RANDOM_STRING}`, values)
+    .then(() => callback())
+
+  return{
+    type: CREATE_POST,
+    payload: request
+  }
+}
+
+export function fetchPost(id){
+  const request = axios.get(`${ROOT_URL}/posts/${id}${RANDOM_STRING}`);
+
+  return{
+    type: FETCH_POST,
     payload: request
   }
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions'
 
 class PostNew extends Component {
   renderField(field){
@@ -23,7 +25,9 @@ class PostNew extends Component {
   }
 
   onSubmit(values){
-  
+    this.props.createPost(values, () =>{
+      this.props.history.push('/')
+    })
   }
 
   render(){
@@ -48,6 +52,7 @@ class PostNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }
@@ -71,4 +76,6 @@ function validate(values){
  export default reduxForm({
    validate,
    form: 'PostsNewForm'
- })(PostNew);
+ })(
+   connect (null, { createPost })(PostNew)
+ );
